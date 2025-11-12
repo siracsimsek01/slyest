65b7c27d1e14ff70974d508f9d483e09adcbd2ed
Htet Aung Hlaing <haunghlaing2112003@gmail.com>
2025-11-12 21:28:38 +0000

    [Driver: hh22704, Navigator: yc21393] Wrote the functions for replacing and removing variable assignments as part of variable management

--- src/app/core/variable_assignment.py
+++ src/app/core/variable_assignment.py
@@ -4,10 +4,28 @@ class VariableManager:
     def __init__(self):
         self.variables = {}
 
-    def assign(self, name: str, expression: str):
-        """Assign a symbolic expression to a variable."""
+    def assign_variable(self, name, expression):
         self.variables[name] = sympify(expression)
 
-    def get(self, name: str):
-        """Return a stored expression by variable name."""
+    def get_variable(self, name):
         return self.variables.get(name)
+    
+    def remove_variable(self, name):
+        try:
+            self.variables.pop(name)
+        except KeyError:
+            print(f'Variable {name} is not defined yet.')
+    
+    def replace_variable(self, name, expression):
+        try:
+            self.variables[name] = sympify(expression)
+        except KeyError:
+            print(f'Variable {name} is not defined yet.')
+
+if __name__ == '__main__':
+    variable_manager = VariableManager()
+    variable_manager.assign_variable('a', 'x + 1')
+    variable_manager.assign_variable('b', 'x + 2')
+    variable_manager.replace_variable('b', 'x**2')
+    variable_manager.remove_variable('a')
+    print(variable_manager.variables)
