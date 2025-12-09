e84eb26c76ac0a8b8d531668165a705654f6181f
Htet Aung Hlaing <haunghlaing2112003@gmail.com>
2025-12-09 16:35:44 +0000

    [Driver: hh22704, Navigator: as23028] Refactored the learning_mode_window by splitting out large functions and creating general functions to remove repeated code

--- src/app/gui/learning_mode_window.py
+++ src/app/gui/learning_mode_window.py
@@ -2,7 +2,6 @@ from PyQt6.QtWidgets import (
     QDialog, QVBoxLayout, QHBoxLayout, QLabel)
 
 class LearningModeWindow(QDialog):
-    """dialog for learning mode"""
     def __init__(self, operation, input_expression, result, optional_input, parent=None):
         super().__init__(parent)
         self.apply_stylesheet()
@@ -12,6 +11,7 @@ class LearningModeWindow(QDialog):
         self.input_expression = input_expression
         self.result = result
         self.optional_input = optional_input
+        self.label_width = 200
         self.initialise_ui()
         
     def apply_stylesheet(self):
@@ -42,61 +42,33 @@ class LearningModeWindow(QDialog):
     def initialise_ui(self):
         layout = QVBoxLayout(self)
         layout.setSpacing(15)
-
-        # name input
         window_layout = QVBoxLayout()
 
-        input_layout_operation = QHBoxLayout()
-        operation = QLabel("Operation: ")
-        operation.setObjectName("name")
-        operation.setFixedWidth(200)
-        operation_input = QLabel(self.operation)
-        operation_input.setObjectName("expression")
-        input_layout_operation.addWidget(operation)
-        input_layout_operation.addWidget(operation_input)
-        input_layout_operation.setContentsMargins(0, 0, 0, 0)
-
-        input_layout_expression = QHBoxLayout()
-        expression = QLabel("Input Expression: ")
-        expression.setObjectName("name")
-        expression.setFixedWidth(200)
-        expression_input = QLabel(self.input_expression)
-        expression_input.setObjectName("expression")
-        input_layout_expression.addWidget(expression)
-        input_layout_expression.addWidget(expression_input)
-
-        input_layout_optional_expression = QHBoxLayout()
-        optional_expression = QLabel("Optional Input Expression: ")
-        optional_expression.setObjectName("name")
-        optional_expression.setFixedWidth(200)
-        optional_expression_input = QLabel(self.optional_input)
-        optional_expression_input.setObjectName("expression")
-        input_layout_optional_expression.addWidget(optional_expression)
-        input_layout_optional_expression.addWidget(optional_expression_input)
-
-        result = QHBoxLayout()
-        result_label = QLabel("Answer: ")
-        result_label.setObjectName("name")
-        result_label.setFixedWidth(200)
-        result_label_input = QLabel(self.result)
-        result_label_input.setObjectName("expression")
-        result.addWidget(result_label)
-        result.addWidget(result_label_input)
+        window_layout.addLayout(self.create_box_layout("Operation", self.operation, self.label_width))
+        window_layout.addLayout(self.create_box_layout("Input Expression: ", self.input_expression, self.label_width))
+        window_layout.addLayout(self.create_box_layout("Optional Input Expression: ", self.optional_input, self.label_width))
+        window_layout.addLayout(self.create_box_layout("Answer: ", self.result, self.label_width))
 
         learning_hint_layout = QVBoxLayout()
-        hinting_label = QLabel("Step-by-step hints: ")
-        hinting_label.setObjectName("name")
+        learning_hint_layout.addWidget(self.create_label("Step-by-step hints: ", "name"))
         learning_hints = QLabel("Placeholder")
         learning_hints.setObjectName("learning")
         learning_hints.setMinimumHeight(200)
         learning_hints.setWordWrap(True)
-        learning_hint_layout.addWidget(hinting_label)
         learning_hint_layout.addWidget(learning_hints)
-
-        window_layout.addLayout(input_layout_operation)
-        window_layout.addLayout(input_layout_expression)
-        window_layout.addLayout(input_layout_optional_expression)
-        window_layout.addLayout(result)
         window_layout.addLayout(learning_hint_layout)
 
         layout.addLayout(window_layout)
+
+    def create_label(self, text, object_name, width=None):
+        label = QLabel(text)
+        label.setObjectName(object_name)
+        if width:
+            label.setFixedWidth(width)
+        return label
+
+    def create_box_layout(self, label_name, label_value, width):
+        layout = QHBoxLayout()
+        layout.addWidget(self.create_label(label_name, "name", width))
+        layout.addWidget(self.create_label(label_value, "expression"))
+        return layout
